1443f3aad29c3fa3e65aae9c2acd44ac1c028c8d
raducubalgiu <balgiu_radu_2007@yahoo.com>
2025-07-28 08:47:31 +0300

    Modifiy Post Shimmer

--- app/src/main/java/com/example/scrollbooker/ui/sharedModules/posts/components/PostShimmer.kt
+++ app/src/main/java/com/example/scrollbooker/ui/sharedModules/posts/components/PostShimmer.kt
@@ -4,41 +4,81 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.scrollbooker.components.core.shimmer.ShimmerMode
 import com.example.scrollbooker.components.core.shimmer.rememberShimmerBrush
+import com.example.scrollbooker.core.util.Dimens.BasePadding
+import com.example.scrollbooker.core.util.Dimens.SpacingM
 import com.example.scrollbooker.core.util.Dimens.SpacingS
 
 @Composable
-fun PostShimmer() {
+fun PostShimmer(
+    paddingBottom: Dp = 90.dp
+) {
     val brush = rememberShimmerBrush(mode = ShimmerMode.DARK)
 
     Box(modifier = Modifier
         .fillMaxSize()
-        .padding(SpacingS),
+        .padding(SpacingS)
+        .padding(bottom = paddingBottom),
         contentAlignment = Alignment.BottomCenter
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.Bottom
-        ) {
+        Column {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Box(
+                    modifier = Modifier
+                        .size(50.dp)
+                        .clip(CircleShape)
+                        .background(brush)
+                )
+
+                Spacer(Modifier.width(SpacingS))
+
+                Column {
+                    Spacer(modifier = Modifier
+                        .fillMaxWidth(fraction = 0.3f)
+                        .height(20.dp)
+                        .clip(shape = ShapeDefaults.Medium)
+                        .background(brush)
+                    )
+
+                    Spacer(Modifier.height(SpacingM))
+
+                    Spacer(modifier = Modifier
+                        .fillMaxWidth(fraction = 0.6f)
+                        .height(20.dp)
+                        .clip(shape = ShapeDefaults.Medium)
+                        .background(brush)
+                    )
+                }
+            }
+
+            Spacer(Modifier.height(BasePadding))
+
             Spacer(modifier = Modifier
                 .fillMaxWidth(fraction = 0.8f)
                 .height(40.dp)
                 .clip(shape = ShapeDefaults.Medium)
                 .background(brush)
             )
+
+            Spacer(Modifier.height(BasePadding))
         }
     }
 }
