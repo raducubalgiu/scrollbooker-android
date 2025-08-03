b687287f864c9b650d843b113f198c66238799b8
raducubalgiu <balgiu_radu_2007@yahoo.com>
2025-08-03 13:47:01 +0300

    Add SafeDrawing in currencies & schedules screens

--- app/src/main/java/com/example/scrollbooker/ui/profile/myProfile/myBusiness/myCurrencies/MyCurrenciesScreen.kt
+++ app/src/main/java/com/example/scrollbooker/ui/profile/myProfile/myBusiness/myCurrencies/MyCurrenciesScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -42,7 +43,7 @@ fun MyCurrenciesScreen(
     val isEnabled = !isLoading && selectedIds.isNotEmpty() && selectedIds != defaultSelectedIds
 
     FormLayout(
-        modifier = Modifier.statusBarsPadding(),
+        modifier = Modifier.safeDrawingPadding(),
         headLine = stringResource(R.string.acceptedCurrencies),
         subHeadLine = stringResource(R.string.chooseDesiredCurrencies),
         buttonTitle = buttonTitle,
