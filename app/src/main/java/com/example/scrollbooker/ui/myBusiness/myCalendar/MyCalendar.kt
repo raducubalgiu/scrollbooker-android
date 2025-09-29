80b378a6c08e93d01ce6cf1b7ee5ad679286ac5c
raducubalgiu <balgiu_radu_2007@yahoo.com>
2025-09-29 18:13:23 +0300

    Initial Commit for MyCalendarScreen

--- app/src/main/java/com/example/scrollbooker/ui/myBusiness/myCalendar/MyCalendar.kt
+++ app/src/main/java/com/example/scrollbooker/ui/myBusiness/myCalendar/MyCalendar.kt
@@ -1,11 +1,181 @@
 package com.example.scrollbooker.ui.myBusiness.myCalendar
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.scrollbooker.R
+import com.example.scrollbooker.components.core.headers.Header
+import com.example.scrollbooker.components.core.layout.ErrorScreen
+import com.example.scrollbooker.components.core.layout.LoadingScreen
+import com.example.scrollbooker.core.util.Dimens.BasePadding
+import com.example.scrollbooker.core.util.FeatureState
+import com.example.scrollbooker.core.util.displayDatePeriod
+import com.example.scrollbooker.core.util.displayShortDayOfWeek
+import com.example.scrollbooker.ui.modules.calendar.components.CalendarDayTab
+import com.example.scrollbooker.ui.modules.calendar.components.CalendarHeader
+import com.example.scrollbooker.ui.theme.OnBackground
+import com.example.scrollbooker.ui.theme.Primary
+import kotlinx.coroutines.launch
+import java.util.Locale
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun MyCalendarScreen(
     viewModel: MyCalendarViewModel,
     onBack: () -> Unit,
     isBusiness: Boolean = false
 ) {
-//    Calendar(onBack = onBack)
+    val headerState by viewModel.calendarHeader.collectAsState()
+
+    val locale = Locale("ro")
+    val coroutineScope = rememberCoroutineScope()
+
+    fun handleDayChange(day: org.threeten.bp.LocalDate) {
+        viewModel.setDay(day)
+    }
+
+    Scaffold(
+        topBar = {
+            Header(
+                title = stringResource(R.string.calendar),
+                onBack = onBack
+            )
+        }
+    ) { innerPadding ->
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(top = innerPadding.calculateTopPadding())
+        ) {
+            when(headerState) {
+                is FeatureState.Error -> ErrorScreen()
+                is FeatureState.Loading -> LoadingScreen()
+                is FeatureState.Success -> {
+                    val calendar = (headerState as FeatureState.Success).data
+                    val config = calendar.config
+                    val calendarDays = calendar.calendarDays
+                    val availableDays = calendar.calendarAvailableDays
+
+                    if(calendar.calendarDays.isEmpty()) {
+                        ErrorScreen()
+                        return@Box
+                    }
+
+                    val weekPagerState = rememberPagerState(initialPage = config.initialWeekPage) { config.totalWeeks }
+                    val dayPagerState = rememberPagerState(initialPage = config.initialDayPage) { 7 }
+
+                    val currentDayIndex = dayPagerState.currentPage
+                    val currentWeekIndex = weekPagerState.currentPage
+
+                    LaunchedEffect(currentWeekIndex, currentDayIndex) {
+                        val dayIndex = currentWeekIndex * 7 + currentDayIndex
+                        val day = calendarDays.getOrNull(dayIndex)
+                        if(day != null) {
+                            handleDayChange(day)
+                        }
+                    }
+
+                    val currentWeekDates = calendarDays.drop(currentWeekIndex * 7).take(7)
+                    val enableBack = currentWeekIndex > 0
+                    val enableNext = currentWeekIndex < config.totalWeeks - 1
+
+                    fun handlePreviousWeek() {
+                        coroutineScope.launch {
+                            weekPagerState.animateScrollToPage(currentWeekIndex - 1)
+                        }
+                    }
+
+                    fun handleNextWeek() {
+                        coroutineScope.launch {
+                            weekPagerState.animateScrollToPage(currentWeekIndex + 1)
+                        }
+                    }
+
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        CalendarHeader(
+                            period = displayDatePeriod(currentWeekDates.first(), currentWeekDates.last(), locale),
+                            enableBack = enableBack,
+                            enableNext = enableNext,
+                            handlePreviousWeek = { handlePreviousWeek() },
+                            handleNextWeek = { handleNextWeek() }
+                        )
+
+                        HorizontalPager(
+                            state = weekPagerState,
+                            pageSize = PageSize.Fill,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(95.dp)
+                                .padding(horizontal = BasePadding),
+                            beyondViewportPageCount = 0
+                        ) { page ->
+                            val weekDates = calendarDays.drop(page * 7).take(7)
+
+                            TabRow(
+                                selectedTabIndex = dayPagerState.currentPage,
+                                modifier = Modifier.fillMaxWidth(),
+                                containerColor = Color.Transparent,
+                                contentColor = OnBackground,
+                                indicator = {},
+                            ) {
+                                weekDates.forEachIndexed { index, date ->
+                                    val isAvailable = availableDays.contains(date) == true
+
+                                    CalendarDayTab(
+                                        date = date,
+                                        isCurrentTab = index == dayPagerState.currentPage,
+                                        onChangeTab = {
+                                            coroutineScope.launch {
+                                                handleDayChange(date)
+                                                dayPagerState.animateScrollToPage(index)
+                                            }
+                                        },
+                                        bgColor = if(currentDayIndex == index) Primary else Color.Transparent,
+                                        label = displayShortDayOfWeek(date, locale),
+                                        isLoading = false,
+                                        isDayAvailable = isAvailable
+                                    )
+                                }
+                            }
+                        }
+
+                        HorizontalPager(
+                            state = dayPagerState,
+                            pageSize = PageSize.Fill,
+                            beyondViewportPageCount = 0,
+                            modifier = Modifier.fillMaxSize()
+                        ) { index ->
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(horizontal = BasePadding),
+                                contentAlignment = Alignment.TopStart
+                            ) {
+                                Text("Something great")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/scrollbooker/ui/myBusiness/myCalendar/MyCalendarViewModel.kt
+++ app/src/main/java/com/example/scrollbooker/ui/myBusiness/myCalendar/MyCalendarViewModel.kt
@@ -1,10 +1,91 @@
 package com.example.scrollbooker.ui.myBusiness.myCalendar
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.scrollbooker.core.util.FeatureState
+import com.example.scrollbooker.core.util.withVisibleLoading
+import com.example.scrollbooker.entity.booking.calendar.domain.useCase.GetCalendarAvailableDaysUseCase
+import com.example.scrollbooker.store.AuthDataStore
+import com.example.scrollbooker.ui.modules.calendar.CalendarConfig
+import com.example.scrollbooker.ui.modules.calendar.CalendarHeaderState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
+import org.threeten.bp.DayOfWeek
+import org.threeten.bp.LocalDate
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class MyCalendarViewModel @Inject constructor(): ViewModel() {
+class MyCalendarViewModel @Inject constructor(
+    private val getCalendarAvailableDaysUseCase: GetCalendarAvailableDaysUseCase,
+    private val authDataStore: AuthDataStore
+): ViewModel() {
+    //private val userId = MutableStateFlow<Int?>(null)
+    private val selectedDay = MutableStateFlow<LocalDate?>(LocalDate.now())
 
+    private fun getCalendarHeader(userId: Int): StateFlow<FeatureState<CalendarHeaderState>> = flow {
+        emit(FeatureState.Loading)
+
+        try {
+            val today = LocalDate.now()
+            val currentMonday = today.with(DayOfWeek.MONDAY)
+
+            val startDate = currentMonday.minusWeeks(13)
+            val endDate = currentMonday.plusWeeks(13)
+
+            val totalWeeks = 26
+            val calendarDays = (0 until (totalWeeks * 7)).map {
+                startDate.plusDays(it.toLong())
+            }
+
+            val availableDays = withVisibleLoading {
+                getCalendarAvailableDaysUseCase(
+                    userId, startDate.toString(), endDate.toString())
+            }
+
+            emit(
+                FeatureState.Success(
+                    CalendarHeaderState(
+                        config = CalendarConfig(
+                            userId = userId,
+                            startDate = startDate,
+                            endDate = endDate,
+                            totalWeeks = totalWeeks,
+                            initialWeekPage = totalWeeks / 2,
+                            initialDayPage = today.dayOfWeek.ordinal,
+                            selectedDay = today
+                        ),
+                        calendarDays = calendarDays,
+                        calendarAvailableDays = availableDays.map {LocalDate.parse(it) },
+                    )
+                )
+            )
+        } catch (e: Exception) {
+            Timber.tag("Calendar").e("ERROR: on Fetching Calendar Header: $e")
+            emit(FeatureState.Error(e))
+        }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.Eagerly,
+        initialValue = FeatureState.Loading
+    )
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val calendarHeader: StateFlow<FeatureState<CalendarHeaderState>> = authDataStore.getUserId()
+        .filterNotNull()
+        .flatMapLatest { userId ->
+            getCalendarHeader(userId)
+        }
+        .stateIn(viewModelScope, SharingStarted.Eagerly, FeatureState.Loading)
+
+    fun setDay(day: LocalDate) {
+        selectedDay.value = day
+    }
 }
