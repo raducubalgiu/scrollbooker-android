69150ee56b2c54d3ff8e86617308cb35b4ea86d9
raducubalgiu <balgiu_radu_2007@yahoo.com>
2026-07-02 21:39:17 +0300

    Improve NotificationsList

--- app/src/main/java/com/example/scrollbooker/ui/inbox/components/NotificationsList.kt
+++ app/src/main/java/com/example/scrollbooker/ui/inbox/components/NotificationsList.kt
@@ -1,29 +1,18 @@
 package com.example.scrollbooker.ui.inbox.components
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ShapeDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
-import com.example.scrollbooker.R
-import com.example.scrollbooker.components.core.buttons.MainButtonSmall
-import com.example.scrollbooker.components.customized.UserListItem
-import com.example.scrollbooker.core.enums.NotificationTypeEnum
 import com.example.scrollbooker.components.customized.LoadMoreSpinner
 import com.example.scrollbooker.components.customized.Refresh
 import com.example.scrollbooker.entity.user.notification.domain.model.Notification
 import com.example.scrollbooker.navigation.navigators.InboxNavigator
 import com.example.scrollbooker.ui.inbox.InboxViewModel
-import com.example.scrollbooker.ui.theme.Divider
-import com.example.scrollbooker.ui.theme.Error
-import com.example.scrollbooker.ui.theme.OnError
-import com.example.scrollbooker.ui.theme.OnSurfaceBG
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -41,27 +30,32 @@ fun NotificationsList(
         isRefreshing = notifications.loadState.refresh is LoadState.Loading,
         onRefresh = { viewModel.refreshNotifications() }
     ) {
-        LazyColumn(Modifier.fillMaxSize()) {
-            items(notifications.itemCount) { index ->
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(
+                count = notifications.itemCount,
+                key = { index -> notifications[index]?.id ?: index }
+            ) { index ->
                 val notification = notifications[index]
 
                 notification?.let { notif ->
                     val senderId = notif.sender.id
                     val isLocked = isFollowSaving.contains(senderId)
+                    val isFollowed = followState[senderId] ?: notif.sender.isFollow
 
                     NotificationItem(
-                        modifier = Modifier.animateItem(),
                         notification = notif,
                         isLocked = isLocked,
-                        isFollowed = followState[senderId],
-                        onFollow = { isFollowed -> onFollow(isFollowed, senderId) },
+                        isFollowed = isFollowed,
+                        onFollow = { isFollowedTarget -> onFollow(isFollowedTarget, senderId) },
                         inboxNavigate = inboxNavigate
                     )
                 }
             }
 
-            item {
-                if (appendState is LoadState.Loading) {
+            if (appendState is LoadState.Loading) {
+                item {
                     LoadMoreSpinner()
                 }
             }
